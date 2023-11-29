c6da87389ce12cf6775ab5a36da23868d68ef96f
Bruno Maruszczak <72678261+bruno-maruszczak@users.noreply.github.com>
2023-11-29 22:45:15 +0100

    Moved stack generator to a different file

--- src/main.zig
+++ src/main.zig
@@ -1,83 +1,5 @@
 const std = @import("std");
-const testing = std.testing;
-
-
-pub fn Stack(comptime T: type) type {
-
-    const StackNode = struct {
-        const Self = @This();
-
-        value: T,
-        next: ?*Self,
-    };
-
-    return struct {
-        const Self = @This();
-        top: ?*StackNode,
-        alloc: std.mem.Allocator,
-
-        fn init(alloc: std.mem.Allocator) Self {
-            return .{
-                .alloc = alloc,
-                .top = null,
-            };
-        }
-
-        fn deinit(self: Self) void {
-            var head = self.top;
-            while (head) |node| {
-                head = node.next;
-                self.alloc.destroy(node);
-            }
-        }
-
-        fn print(self: *const Self) void {
-            std.log.info("Stack:", .{});
-            var head = self.top;
-            var idx: u32 = 0;
-            while (head) |node| {
-                std.log.info("{d} ~> {d}", .{ idx, node.value });
-                idx = idx + 1;
-                head = node.next;
-            }
-        }
-
-        fn isEmpty(self: *Self) bool {
-            if(self.top) |_| {
-                return false;
-            } else {
-                return true;
-            }
-        }
-
-        fn push(self: *Self, value: T) !void {
-            var node = try self.alloc.create(StackNode);
-            errdefer self.alloc.destroy(node);
-
-            node.value = value;
-            var top = self.top;
-            node.next = top;
-
-            self.top = node;
-        }
-
-        fn pop(self: *Self) !void {
-            if(self.top) |top| {
-                self.top = top.next;
-                self.alloc.destroy(top);
-            } else {
-                return error.StackEmpty;
-            }
-        }
-
-        fn peek(self: *Self) ?T {
-            if (self.top) |node| {
-                return node.value;
-            }
-            return null;
-        }
-    };
-}
+const intStack = @import("./Stack.zig").Stack(i64);
 
 
 
@@ -85,7 +7,7 @@ pub fn main() !void {
     // var
     var gp = std.heap.GeneralPurposeAllocator(.{}){};
     var alloc = gp.allocator();
-    var stack = Stack(i64).init(alloc);
+    var stack = intStack.init(alloc);
     try stack.push(5);
     try stack.push(7);
     try stack.push(9);
@@ -97,21 +19,3 @@ pub fn main() !void {
 
 }
 
-test "push and peek works" {
-    var stack = Stack(i64).init(std.testing.allocator);
-    defer stack.deinit();
-
-    try stack.push(5);
-    
-    if (stack.peek()) |val| {
-        try testing.expect(val == 5);
-    } else {
-        return error.StackEmptyAfterPush;
-    }
-}
-
-test "pop errors on empty" {
-    var stack = Stack(void).init(std.testing.allocator);
-    defer stack.deinit();
-    try testing.expectError(error.StackEmpty, stack.pop());
-}
---
+++ src/stack.zig
@@ -0,0 +1,103 @@
+const std = @import("std");
+const testing = std.testing;
+
+
+pub fn Stack(comptime T: type) type {
+
+    const StackNode = struct {
+        const Self = @This();
+
+        value: T,
+        next: ?*Self,
+    };
+
+    return struct {
+        const Self = @This();
+        top: ?*StackNode,
+        alloc: std.mem.Allocator,
+
+        pub fn init(alloc: std.mem.Allocator) Self {
+            return .{
+                .alloc = alloc,
+                .top = null,
+            };
+        }
+
+        pub fn deinit(self: Self) void {
+            var head = self.top;
+            while (head) |node| {
+                head = node.next;
+                self.alloc.destroy(node);
+            }
+        }
+
+        pub fn isEmpty(self: *Self) bool {
+            if(self.top) |_| {
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+        pub fn push(self: *Self, value: T) !void {
+            var node = try self.alloc.create(StackNode);
+            errdefer self.alloc.destroy(node);
+
+            node.value = value;
+            var top = self.top;
+            node.next = top;
+
+            self.top = node;
+        }
+
+        pub fn pop(self: *Self) !void {
+            if(self.top) |top| {
+                self.top = top.next;
+                self.alloc.destroy(top);
+            } else {
+                return error.StackEmpty;
+            }
+        }
+
+        pub fn peek(self: *Self) ?T {
+            if (self.top) |node| {
+                return node.value;
+            }
+            return null;
+        }
+
+
+        pub fn print(self: *const Self) void {
+            std.log.info("Stack:", .{});
+            var head = self.top;
+            var idx: u32 = 0;
+            while (head) |node| {
+                std.log.info("{d} ~> {d}", .{ idx, node.value });
+                idx = idx + 1;
+                head = node.next;
+            }
+        }
+    };
+}
+
+
+
+
+test "push and peek works" {
+    var stack = Stack(i64).init(std.testing.allocator);
+    defer stack.deinit();
+
+    try stack.push(5);
+    
+    if (stack.peek()) |val| {
+        try testing.expect(val == 5);
+    } else {
+        return error.StackEmptyAfterPush;
+    }
+}
+
+test "pop errors on empty" {
+    var stack = Stack(void).init(std.testing.allocator);
+    defer stack.deinit();
+    try testing.expectError(error.StackEmpty, stack.pop());
+}
